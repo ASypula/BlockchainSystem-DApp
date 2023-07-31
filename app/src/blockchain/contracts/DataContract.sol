90f958221fe73d75beafb9d8e2714161339ea7d4
asypula <a.sypula@stud.elka.pw.edu.pl>
2023-07-31 22:31:31 +0200

    Default select options for dropdowns and refreshes

--- app/src/blockchain/contracts/DataContract.sol
+++ app/src/blockchain/contracts/DataContract.sol
@@ -5,6 +5,7 @@ contract DataContract {
 
   event Log(string text);
   event Debug(string element, string value);
+  event LogNr (uint nr);
 
   struct Record {
     uint date;
@@ -18,7 +19,9 @@ contract DataContract {
   // SHIP functions
 
   function addShip(string memory name) public {
+    emit Log("In addShip");
     require(!contains(shipNames, name), "Error: Ship with provided name already exists.");
+    emit Debug("Ship added", name);
     shipNames.push(name);
   }
 
@@ -33,7 +36,9 @@ contract DataContract {
   // PART functions
 
   function addPart(string memory shipName, string memory partName) public {
+    emit Log("In addPart");
     require(!contains(partNames[shipName], partName), "Error: Part with provided name already exists for this ship.");
+    emit Debug("Part added", partName);
     partNames[shipName].push(partName);
   }
 
@@ -44,18 +49,23 @@ contract DataContract {
   // RECORD functions
 
   function addRecord(string memory shipName, string memory partName, uint date, string memory descr) public {
+    emit Log("In Add Record");
     require(contains(partNames[shipName], partName), "Error: Part for given ship does not exist.");
     Record memory newEntry = Record(date, descr);
     allRecords[shipName][partName].push(newEntry);
     emit Log("New record added");
   }
 
-  function getLatestRecords(string memory ship) public view returns (string[] memory, Record[] memory){
+// add view to function declaration
+  function getLatestRecords(string memory ship) public returns (string[] memory, Record[] memory){
+    emit Log("In getLatestRecords");
     Record[] memory latestRecords = new Record[](partNames[ship].length);
+    emit LogNr(partNames[ship].length);
     // For each part in the ship get the latest record (the last in the array)
     for (uint256 i = 0 ; i < partNames[ship].length ; i++) {
       uint256 lastIdx = allRecords[ship][partNames[ship][i]].length - 1;
       latestRecords[i] = allRecords[ship][partNames[ship][i]][lastIdx];
+      emit Debug(ship, allRecords[ship][partNames[ship][i]][lastIdx].descr);
     }
     return (partNames[ship], latestRecords);
   }
