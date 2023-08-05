a4ce52cc27efeb9afd70c4006fc919de6d72c255
asypula <a.sypula@stud.elka.pw.edu.pl>
2023-08-05 15:01:20 +0200

    Comments and logs added to contracts

--- app/src/blockchain/contracts/AccountContract.sol
+++ app/src/blockchain/contracts/AccountContract.sol
@@ -1,6 +1,11 @@
 pragma solidity ^0.8.2;
 pragma abicoder v2;
 
+/*
+ * Contract for storing addresses of accounts that are authorized
+ * to use the application ShipParts
+ * Core state variable - accounts
+ */
 contract AccountContract {
 
     event Log(string text);
@@ -8,7 +13,7 @@ contract AccountContract {
    string[] public accounts = ["0x74727Ba4a34cC22203196bEda335f276fb1C52bC"];
 
    function getAllAccounts() public returns (string[] memory){
-        emit Log("In getting accounts");
+    emit Log("Preparing to load all permitted accounts");
     return accounts;
    }
 
--- app/src/blockchain/contracts/DataContract.sol
+++ app/src/blockchain/contracts/DataContract.sol
@@ -1,17 +1,26 @@
 pragma solidity ^0.8.2;
 pragma abicoder v2;
 
+/*
+ * Contract for storing data about maintenance records
+ * Immutability is granted by using the state variables as a database
+ * Core state variable:
+      * shipName
+      * partNames
+      * allRecords
+ */
 contract DataContract {
 
-  event Log(string text);
-  event Debug(string element, string value);
-  event LogNr (uint nr);
-
+  // Each record has a date, description and file stored on the blockchain
+  // File is being represented as a string and upon downloading by a user converted to a plain file
   struct Record {
     uint date;
     string descr;
     string file;
   }
+  
+  event Log(string text);
+  event LogData(string text, string value);
 
   string[] public shipNames;
   mapping(string=>string[]) partNames;
@@ -20,54 +29,55 @@ contract DataContract {
   // SHIP functions
 
   function addShip(string memory name) public {
-    emit Log("In addShip");
+    emit LogData("Preparing to add a new ship", name);
     require(!contains(shipNames, name), "Error: Ship with provided name already exists.");
-    emit Debug("Ship added", name);
     shipNames.push(name);
+    emit LogData("Ship added successfully", name);
   }
 
-  function getShipName(uint idx) public view returns (string memory) {
+  function getShipName(uint idx) public returns (string memory) {
+    emit Log("Preparing to get a ship");
     return shipNames[idx];
   }
 
-  function getAllShips() public view returns (string[] memory){
+  function getAllShips() public returns (string[] memory){
+    emit Log("Preparing to get all ships");
     return shipNames;
   }
 
   // PART functions
 
   function addPart(string memory shipName, string memory partName) public {
-    emit Log("In addPart");
+    emit LogData("Preparing to add a new part", partName);
     require(!contains(partNames[shipName], partName), "Error: Part with provided name already exists for this ship.");
-    emit Debug("Part added", partName);
     partNames[shipName].push(partName);
+    emit LogData("Part added successfully to ship", partName);
   }
 
-  function getAllShipParts(string memory shipName) public view returns (string[] memory){
+  function getAllShipParts(string memory shipName) public returns (string[] memory){
+    emit LogData ("Preparing to load all ship's parts", shipName);
     return partNames[shipName];
   }
 
   // RECORD functions
 
   function addRecord(string memory shipName, string memory partName, uint date, string memory descr, string memory file) public {
-    emit Log("In Add Record");
+    emit LogData("Preparing to add a new record to", partName);
     require(contains(partNames[shipName], partName), "Error: Part for given ship does not exist.");
     Record memory newEntry = Record(date, descr, file);
     allRecords[shipName][partName].push(newEntry);
-    emit Log("New record added");
+    emit Log("New record added successfully");
   }
 
-// add view to function declaration
   function getLatestRecords(string memory ship) public returns (string[] memory, Record[] memory){
-    emit Log("In getLatestRecords");
+    emit Log("Preparing to load latest records for ship");
     Record[] memory latestRecords = new Record[](partNames[ship].length);
-    emit LogNr(partNames[ship].length);
     // For each part in the ship get the latest record (the last in the array)
     for (uint256 i = 0 ; i < partNames[ship].length ; i++) {
       uint256 lastIdx = allRecords[ship][partNames[ship][i]].length - 1;
       latestRecords[i] = allRecords[ship][partNames[ship][i]][lastIdx];
-      emit Debug(ship, allRecords[ship][partNames[ship][i]][lastIdx].descr);
     }
+    emit Log("Latest records loaded successfully");
     return (partNames[ship], latestRecords);
   }
 
